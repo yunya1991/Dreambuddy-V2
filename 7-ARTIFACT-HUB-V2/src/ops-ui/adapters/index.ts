01f07773305252e681178a7adbaa08e953467ace
yunya1991 <yunya1991@gmail.com>
2026-06-15 00:12:07 +0800

    chore: re-trigger CI checks after updating Task Card in PR body

--- 7-ARTIFACT-HUB-V2/src/ops-ui/adapters/index.ts
+++
@@ -1,8 +0,0 @@
-export { HealthContractV1, HealthViewModel, toHealthViewModel } from "./healthAdapter.js";
-export { TraceContractV1, TraceViewModel, toTraceViewModel } from "./traceAdapter.js";
-export {
-  WorkflowContractV1,
-  WorkflowViewModel,
-  ChainPhase,
-  toWorkflowViewModel,
-} from "./workflowAdapter.js";
